af79330a20910d9b0eaed8d0bf33961fd10077ef
mihalytorley <mihaly.torley@bjss.com>
2025-06-16 13:15:16 +0100

    Added input stuff, gonna replace them with flags

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import(
 "fmt"
+"flag"
 )
+
+
 
 func main() {
     fmt.Println("!... Hello World ...!")
+    myslice := []string{}
+    var input string = "start"
+    for input != "exit" {
+        fmt.Scan(&input)
+        myslice = append(myslice, input)
+    }
+    fmt.Println("myslice has value ", myslice)
+}
+
+var flagvar int
+
+func flag_test() {
+    flag.IntVar(&flagvar, "flagname", 1234, "help message for flagname")
 }
